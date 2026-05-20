11fee092e221f299bef258c75a9f4c5b5cbec8d0
Giovanni de Souza Gomes <giovannisouzinha@gmail.com>
2026-05-20 13:58:03 -0300

    Adicionar teste automatizado de login com senha inválida

--- automacao/playwright/tests/login.spec.js
+++ automacao/playwright/tests/login.spec.js
@@ -8,4 +8,15 @@ test.describe('Testes de Login - BugBank', () => {
 
     await expect(page.getByText('É campo obrigatório').first()).toBeVisible();
   });
+
+  test('Login com senha inválida', async ({ page }) => {
+    await page.goto('https://bugbank.netlify.app/');
+
+    await page.locator('input[name="email"]').fill('giovanni.qa.teste@email.com');
+    await page.locator('input[name="password"]').fill('SenhaErrada@123');
+
+    await page.getByRole('button', { name: 'Acessar' }).click();
+
+    await expect(page.getByText('Usuário ou senha inválido.')).toBeVisible();
+  });
 });
