05b4fa2489d68c6747e58e0af2f7af68f0b9197b
Giovanni de Souza Gomes <giovannisouzinha@gmail.com>
2026-05-20 15:51:29 -0300

    Atualiza teste inicial de login com Playwright

--- automacao/playwright/tests/login.spec.js
+++ automacao/playwright/tests/login.spec.js
@@ -1,17 +1,17 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('Testes de Login - BugBank', () => {
-  test('Login com campos vazios', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://bugbank.netlify.app/');
+  });
 
+  test('Deve exibir mensagem de obrigatoriedade ao tentar acessar com campos vazios', async ({ page }) => {
     await page.getByRole('button', { name: 'Acessar' }).click();
 
     await expect(page.getByText('É campo obrigatório').first()).toBeVisible();
   });
 
-  test('Login com senha inválida', async ({ page }) => {
-    await page.goto('https://bugbank.netlify.app/');
-
+  test('Deve exibir mensagem de erro ao tentar acessar com senha inválida', async ({ page }) => {
     await page.locator('input[name="email"]').fill('giovanni.qa.teste@email.com');
     await page.locator('input[name="password"]').fill('SenhaErrada@123');
 
@@ -20,9 +20,7 @@ test.describe('Testes de Login - BugBank', () => {
     await expect(page.getByText('Usuário ou senha inválido.')).toBeVisible();
   });
 
-  test('Login com sucesso', async ({ page }) => {
-    await page.goto('https://bugbank.netlify.app/');
-
+  test('Deve realizar login com sucesso com usuário válido', async ({ page }) => {
     await page.locator('input[name="email"]').fill('giovanni.qa.teste@email.com');
     await page.locator('input[name="password"]').fill('Teste@123');
 
